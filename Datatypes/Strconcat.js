e5dc4066b0517e26bc8d1ad6a62814b9ae193bac
ZenorthXR <6406021631035@fitm.kmutnb.ac.th>
2022-12-22 09:29:29 +0700

    datatypes

--- Datatypes/Strconcat.js
+++ Datatypes/Strconcat.js
@@ -9,7 +9,7 @@ console.log(str1 + "Big" + str2);
 
 console.log("\n********Adding Spacing********\n");
 //Adding Spacing 
-str1 = "Hello";
+str1 = "Hello ";
 
 console.log(str1 + str2);
-console.log(str1 + "Big" + str2);
+console.log(str1 + "Big " + str2);
