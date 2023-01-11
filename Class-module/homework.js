8501c957386b6b50aea628408abbe4806a6ae46b
ZenorthXR <120072893+ZenorthXR@users.noreply.github.com>
2023-01-11 14:24:14 +0700

    homework

--- Class-module/homework.js
+++ Class-module/homework.js
@@ -1,8 +1,32 @@
-const value = '1,2,3,4,5,6,7,8,9'
-function checkDigit(Digit) {
-    let temp = Digit.split('');
+//Program for checking for missing digit numbers [0-9] in a string
+//example: 123456789 has  missing digit is 0 
+//example: 53657687691428890 is not missing any digit
+//example: 1981023568910 has missing digits 4, 7
 
-    return temp
+
+const numStr1 = "123456789";
+const numStr2 = "53657687691428890";
+const numStr3 = "1981023568910";
+
+
+function missingDigits(numStr) {
+    let NoMissing = "no digit missing";
+    let temp = [];
+    let count = 0;
+    for (let n = 0; n < 10; n++) {
+        if (numStr.includes(n)) {
+            n++;
+        } else { temp[count] = n ; count++; }
+    }
+    switch (numStr) {
+        case numStr2: return `${numStr2} ${NoMissing} is not missing any digit`; 
+        case numStr1: return `${numStr} has missing digit is ${temp}`;
+        case numStr3: {
+            let op = temp[0] + ', ' + temp[1]
+            return `${numStr} has missing digits ${op}`;}
+    }
 }
 
-console.log(checkDigit(value));
+console.log(missingDigits(numStr1));
+console.log(missingDigits(numStr2));
+console.log(missingDigits(numStr3));
